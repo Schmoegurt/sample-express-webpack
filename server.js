e6470d1f80742c97b36c87b4b24f6a1f0f2d547a
Kevin <kan.kevin@gmail.com>
2017-03-06 21:11:00 -0500

    Add sample api

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ var server = express();
 // Serve static files, including the Vue application
 server.use(express.static('public'));
 
+server.get("/api", function(req, res) {
+	return res.status(200).send({
+		data: "Hello from the back-end!"
+	});
+});
+
 // Start listening for requests
 server.listen(5000, function(error) {
 	if (error) { throw error; }
--- vue-src/Bar.vue
+++ vue-src/Bar.vue
@@ -1,5 +1,27 @@
 <template>
 	<div>
-		<p>Bar!</p>
+		<p class="f4 lh-copy measure dark-pink">Foo!</p>
+		<p class="f4 lh-copy measure">{{ message }}</p>
 	</div>
 </template>
+
+<script>
+module.exports = {
+	name: 'Foo',
+	data: function() {
+		return {
+			message: ''
+		};
+	},
+	created: function() {
+		var self = this;
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", "./api");
+		xhr.onload = function() {
+			self.message = JSON.parse(xhr.responseText);
+			console.log(self.message);
+		}
+		xhr.send();
+	}
+};
+</script>
